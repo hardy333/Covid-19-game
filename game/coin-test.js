6878cd632385297bef287518d9cd4d102c2c37eb
unknown <samsonidze14@gmail.com>
2024-07-24 18:07:40 +0400

    coin test|

---
+++ game/coin-test.js
@@ -0,0 +1,9 @@
+const coinTestBtn = document.querySelector(".text-coin-btn")
+
+const coinTestDiv = document.querySelector(".example-coin")
+
+
+
+coinTestBtn.addEventListener("click", () => {
+    coinTestDiv.classList.add("animate")
+})
